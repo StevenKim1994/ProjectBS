dd8753ddc5b7fa33811ce03b43cf8d1d0a48a5bc
김시윤 <enqueuedequeue@naver.com>
2025-10-27 15:33:59 +0900

    적생성 시스템 추가중

---
+++ Assets/Scripts/System/EnermySystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+using BS.Common;
+using BS.GameObjects;
+using System;
+
+namespace BS.System
+{ 
+    public class EnermySystem : ISystem
+    {
+        private static EnermySystem _instance;
+
+        public static EnermySystem Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = SystemGameObject.Instance.GetSystem<EnermySystem>();
+                }
+                return _instance;
+            }
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Unload()
+        {
+
+        }
+    }
+}
