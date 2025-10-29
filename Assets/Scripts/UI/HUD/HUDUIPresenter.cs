e636b560bcb759009366223565cc986ff7dfb3cb
김시윤 <enqueuedequeue@naver.com>
2025-10-29 17:41:32 +0900

    킬카운트 폰트사이즈 연출

--- Assets/Scripts/UI/HUD/HUDUIPresenter.cs
+++ Assets/Scripts/UI/HUD/HUDUIPresenter.cs
@@ -1,4 +1,5 @@
 ﻿using BS.System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,12 @@ namespace BS.UI
     public class HUDUIPresenter : AbstractUIPresenter<HUDUIView>
     {
         private const string KILL_COUNT_TEXT_FORMAT = "Kills: {0}";
+
+        private const float KILL_FONT_DELTA = 8f;
+        private const float KILL_FONT_UP_DURATION = 0.12f;
+        private const float KILL_FONT_DOWN_DURATION = 0.12f;
+
+        private Sequence _killCountFontSequence;
 
         public override void Init(AbstractUIView bindView)
         {
@@ -27,8 +34,23 @@ namespace BS.UI
 
         public void AddKillCount(int addCount)
         {
-            DataSystem.Instance.PlayerHighScore = DataSystem.Instance.PlayerHighScore+ addCount;
+            if (_killCountFontSequence != null && _killCountFontSequence.IsActive())
+            {
+                _killCountFontSequence.Kill(true);
+                _killCountFontSequence = null;
+            }
+
+            DataSystem.Instance.PlayerHighScore = DataSystem.Instance.PlayerHighScore + addCount;
             _view.KillCountText.SetText(string.Format(KILL_COUNT_TEXT_FORMAT, DataSystem.Instance.PlayerHighScore));
+
+            float baseSize = _view.KillCountText.fontSize;
+            float targetSize = baseSize + KILL_FONT_DELTA;
+
+            _killCountFontSequence = DOTween.Sequence();
+            _killCountFontSequence
+                .Append(_view.KillCountText.DOFontSize(targetSize, KILL_FONT_UP_DURATION).SetEase(Ease.OutQuad))
+                .Append(_view.KillCountText.DOFontSize(baseSize, KILL_FONT_DOWN_DURATION).SetEase(Ease.InQuad))
+                .SetUpdate(false);
         }
     }
 }
