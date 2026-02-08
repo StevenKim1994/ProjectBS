d2ddfa39a8a38ad5adbca9e217c19d5211652c53
steven1994 <enqueuedequeue@naver.com>
2026-02-08 10:14:55 +0900

    Create DialogueNode.cs file

---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,9 @@
+// DialogueNode.cs
+
+// This is a placeholder for the DialogueNode class.
+// Implement your functionality here.
+
+public class DialogueNode
+{
+    // Properties and methods for the DialogueNode.
+}
